068b1f73fd1cbd066884ce0159dc79680590f679
yuhueilee <wendylee70127@gmail.com>
2024-05-09 13:43:27 +0800

    Implements the query with dummy data

--- server/src/resolver.js
+++ server/src/resolver.js
@@ -1,5 +1,30 @@
 export const resolvers = {
     Query: {
         appName: () => "ProductHunt clone", // implements the appName query
+        allProducts: () => productsData,
     },
 };
+
+const productsData = [
+    {
+        name: "Educative",
+        description: "Interactive Courses for Software Developers",
+        url: "https://educative.io/",
+        numberOfVotes: 10,
+        publishedAt: "2021-04-05",
+    },
+    {
+        name: "Apollo",
+        description: "The Apollo Data Graph Platform",
+        url: "https://www.apollographql.com/",
+        numberOfVotes: 5,
+        publishedAt: "2021-01-08",
+    },
+    {
+        name: "OneGraph",
+        description: "Build Integrations 100x Faster",
+        url: "https://www.onegraph.com",
+        numberOfVotes: 5,
+        publishedAt: "2020-08-22",
+    },
+];
