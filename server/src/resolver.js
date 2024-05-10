033b6c8f1ab0a878169ec1ea31308d9b7a60553f
yuhueilee <wendylee70127@gmail.com>
2024-05-10 09:51:58 +0800

    Refactor code to reduce duplication of users data

--- server/src/resolver.js
+++ server/src/resolver.js
@@ -3,6 +3,12 @@ export const resolvers = {
         appName: () => "ProductHunt clone", // implements the appName query
         allProducts: () => productsData,
     },
+    // Specifies how to get fields for the "Product" type
+    Product: {
+        author: (product) => {
+            return usersData.find((user) => user.id === product.authorId);
+        },
+    },
 };
 
 const productsData = [
@@ -12,11 +18,7 @@ const productsData = [
         url: "https://educative.io/",
         numberOfVotes: 10,
         publishedAt: "2021-04-05",
-        author: {
-            id: "1",
-            userName: "ellen",
-            fullName: "Ellen James",
-        },
+        authorId: "1",
     },
     {
         name: "Apollo",
@@ -24,11 +26,7 @@ const productsData = [
         url: "https://www.apollographql.com/",
         numberOfVotes: 5,
         publishedAt: "2021-01-08",
-        author: {
-            id: "2",
-            userName: "peter",
-            fullName: "Peter Miles",
-        },
+        authorId: "2",
     },
     {
         name: "OneGraph",
@@ -36,10 +34,19 @@ const productsData = [
         url: "https://www.onegraph.com",
         numberOfVotes: 5,
         publishedAt: "2020-08-22",
-        author: {
-            id: "1",
-            userName: "ellen",
-            fullName: "Ellen James",
-        },
+        authorId: "1",
+    },
+];
+
+const usersData = [
+    {
+        id: "1",
+        userName: "ellen",
+        fullName: "Ellen James",
+    },
+    {
+        id: "2",
+        userName: "peter",
+        fullName: "Peter Miles",
     },
 ];
